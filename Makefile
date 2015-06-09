8796dc2c498871c9206ac3c1907cb889f73b6300
Jeff Lee <jeffomatic@gmail.com>
2015-06-09 00:04:01 -0700

    Add demo code for PA and SDL

--- Makefile
+++ Makefile
@@ -4,23 +4,31 @@ SOURCEDIR = src
 BUILDDIR = build
 VENDORDIR = vendor
 
+SDL_DIR = $(VENDORDIR)/sdl
+SDL_LIB = $(SDL_DIR)/build/.libs/libSDL2.a
+SDL_FRAMEWORKS = ForceFeedback IOKit AppKit
+SDL_LIB_DEPS = iconv
+
 PA_DIR = $(VENDORDIR)/portaudio
-PA_LIB = $(PA_DIR)/lib/.libs
+PA_LIB = $(PA_DIR)/lib/.libs/libportaudio.a
 PA_FRAMEWORKS = CoreAudio AudioToolbox AudioUnit CoreServices Carbon
 
-FRAMEWORKS = $(PA_FRAMEWORKS)
+LIB_DEPS = $(SDL_LIB_DEPS)
+FRAMEWORKS = $(PA_FRAMEWORKS) $(SDL_FRAMEWORKS)
+
+LIB_DEP_FLAGS = $(patsubst %,-l%,$(LIB_DEPS))
 FRAMEWORK_FLAGS = $(patsubst %,-framework %,$(FRAMEWORKS))
 
 SOURCES = $(wildcard $(SOURCEDIR)/*.cpp)
 OBJECTS = $(patsubst $(SOURCEDIR)/%.cpp,$(BUILDDIR)/%.o,$(SOURCES))
-VENDORDEPS = $(PA_LIB)
+VENDORDEPS = $(PA_LIB) $(SDL_LIB)
 
 EXECUTABLE = loop
 
 all: $(BUILDDIR)/$(EXECUTABLE)
 
 $(BUILDDIR)/$(EXECUTABLE) : $(OBJECTS) $(VENDORDEPS)
-	$(CC) $(OBJECTS) $(PA_LIB)/libportaudio.a $(FRAMEWORK_FLAGS) -o $@
+	$(CC) $(OBJECTS) $(PA_LIB) $(SDL_LIB) $(LIB_DEP_FLAGS) $(FRAMEWORK_FLAGS) -o $@
 
 $(OBJECTS): $(SOURCES)
 	$(CC) -g -c $< -o $@
@@ -28,10 +36,18 @@ $(OBJECTS): $(SOURCES)
 $(PA_LIB):
 	cd $(PA_DIR); ./configure && make
 
+$(SDL_LIB):
+	cd $(SDL_DIR); ./configure && make
+
 clean-all: clean vendorclean
 
 clean:
 	rm -rf $(BUILDDIR)/*
 
-vendorclean:
+vendorclean: pa-clean sdl-clean
+
+pa-clean:
 	cd $(PA_DIR); make clean
+
+sdl-clean:
+	cd $(SDL_DIR); make clean
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "../vendor/portaudio/include/portaudio.h"
+#include "../vendor/sdl/include/SDL.h"
 
-int main() {
-	printf("Hello world!, %p\n", &Pa_GetDefaultHostApi);
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+void dieOnPaErr(PaError err, const char* context) {
+	if (err == paNoError) return;
+
+	printf("%s paError: %s", context, Pa_GetErrorText(err));
+	exit(1);
+}
+
+int main(int argc, char* args[]) {
+	PaError paErr = Pa_Initialize();
+	dieOnPaErr(paErr, "Pa_Initialize");
+
+	const int nDevices = Pa_GetDeviceCount();
+	if (nDevices < 0) dieOnPaErr((PaError)nDevices, "Pa_GetDeviceCount");
+
+	printf("Audio devices: %d\n", nDevices);
+
+	for (int i = 0; i < nDevices; i++) {
+	    const PaDeviceInfo* deviceInfo = Pa_GetDeviceInfo(i);
+
+	    printf("\n");
+	    printf("Device %d: %s\n", i, deviceInfo->name);
+	    printf("Max channels:\n");
+	    printf("- input:\t%d\n", deviceInfo->maxInputChannels);
+	    printf("- output:\t%d\n", deviceInfo->maxOutputChannels);
+	    printf("Default latency:\n");
+	    printf("- low input:\t%f\n", deviceInfo->defaultLowInputLatency);
+	    printf("- low output:\t%f\n", deviceInfo->defaultLowOutputLatency);
+	    printf("- high input:\t%f\n", deviceInfo->defaultHighInputLatency);
+	    printf("- high output:\t%f\n", deviceInfo->defaultHighOutputLatency);
+	    printf("Default sample rate: %f\n", deviceInfo->defaultSampleRate);
+	}
+
+	paErr = Pa_Terminate();
+	dieOnPaErr(paErr, "Pa_Terminate");
+
+    if (SDL_Init( SDL_INIT_VIDEO ) < 0) {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+    } else {
+        SDL_Window* window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if (window == NULL) {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+        } else {
+            SDL_Surface* screenSurface = SDL_GetWindowSurface( window );
+		    SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+            SDL_UpdateWindowSurface( window );
+            SDL_Delay(2000);
+        }
+    }
+
 	return 0;
 }
