2981cdbfc9c70f2e703fd92bd35babd999fccd02
Jeff Lee <jeffomatic@gmail.com>
2015-06-14 14:29:58 -0700

    add SpscStream and tests

---
+++ src/util/spsc_stream.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <stdint.h>
+#include <string.h>
+
+#include "basicmath.h"
+
+template<typename T, size_t BIN_CAPACITY>
+struct SpscStream {
+	enum { CAPACITY = 1 << BIN_CAPACITY };
+
+	size_t capacity() const { return CAPACITY; }
+	size_t size() const { return writePos - readPos; }
+
+	size_t read(T* dst, size_t dstCap, size_t amount) {
+		const size_t toRead = min(dstCap, min(size(), amount));
+		const size_t i = readIndex();
+		const size_t toEnd = CAPACITY - i;
+
+		if (toRead > toEnd) {
+			memcpy(dst, &data[i], sizeof(T) * toEnd);
+			memcpy(dst + toEnd, &data[0], sizeof(T) * (toRead - toEnd));
+		} else {
+			memcpy(dst, &data[i], sizeof(T) * toRead);
+		}
+
+		readPos += toRead;
+		return toRead;
+	}
+
+	size_t readAll(T* dst, size_t dstCap) { return read(dst, dstCap, size()); }
+
+	size_t write(const T* src, size_t amount) {
+		const size_t toWrite = min(CAPACITY - size(), amount);
+		const size_t i = writeIndex();
+		const size_t toEnd = CAPACITY - i;
+
+		if (toWrite > toEnd) {
+			memcpy(&data[i], src, sizeof(T) * toEnd);
+			memcpy(&data[0], src + toEnd, sizeof(T) * (toWrite - toEnd));
+		} else {
+			memcpy(&data[i], src, sizeof(T) * toWrite);
+		}
+
+		writePos += toWrite;
+		return toWrite;
+	}
+
+	// Internals
+	T data[CAPACITY];
+	size_t writePos = 0;
+	size_t readPos = 0;
+
+	size_t readIndex() const { return readPos & (CAPACITY - 1); }
+	size_t writeIndex() const { return writePos & (CAPACITY - 1); }
+};
---
+++ src/util/spsc_stream_test.cpp
@@ -0,0 +1,99 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "spsc_stream.h"
+
+void testSpscStream() {
+	SpscStream<double, 4> s;
+	size_t n;
+	double dst[16];
+
+	// Reading out of an empty stream
+	assert(s.capacity() == 16);
+	assert(s.size() == 0);
+	n = s.read(dst, arraySize(dst), arraySize(dst));
+	assert(n == 0);
+
+	// Writing and reading a single element.
+	const double a[] = {1.1};
+	n = s.write(a, arraySize(a));
+	assert(n == 1);
+	assert(s.size() == 1);
+
+	n = s.read(dst, arraySize(dst), arraySize(dst));
+	assert(n == 1);
+	assert(s.size() == 0);
+	for (int i = 0; i < 1; i++) assert(a[i] == dst[i]);
+
+	// Fill the stream to the back of the buffer
+	const double b[] = {2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.10, 11.11, 12.12, 13.13, 14.14, 15.15, 16.16};
+	n = s.write(b, arraySize(b));
+	assert(n == 15);
+	assert(s.size() == 15);
+
+	n = s.read(dst, arraySize(dst), arraySize(dst));
+	assert(n == 15);
+	assert(s.size() == 0);
+	for (int i = 0; i < 15; i++) assert(b[i] == dst[i]);
+
+	// Next write should begin writing at the beginning of the buffer.
+	const double c[] = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8};
+	n = s.write(c, arraySize(c));
+	assert(n == 8);
+	assert(s.size() == 8);
+
+	n = s.read(dst, arraySize(dst), arraySize(dst));
+	assert(n == 8);
+	assert(s.size() == 0);
+	for (int i = 0; i < 8; i++) assert(c[i] == dst[i]);
+
+	// Next write should cause wraparound
+	const double d[] = {
+		1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8,
+		9.9, 10.10, 11.11, 12.12, 13.13, 14.14, 15.15, 16.16,
+	};
+	n = s.write(d, arraySize(d));
+	assert(n == 16);
+	assert(s.size() == 16);
+
+	n = s.read(dst, arraySize(dst), arraySize(dst));
+	assert(n == 16);
+	assert(s.size() == 0);
+	for (int i = 0; i < 16; i++) assert(d[i] == dst[i]);
+
+	// Now read in successive chunks
+	const double e[] = {
+		1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8,
+		9.9, 10.10, 11.11, 12.12, 13.13, 14.14, 15.15, 16.16,
+	};
+	n = s.write(e, arraySize(e));
+	assert(n == 16);
+	assert(s.size() == 16);
+
+	n = s.readAll(dst, 1);
+	assert(n == 1);
+	assert(s.size() == 15);
+	for (int i = 0; i < n; i++) assert(dst[i] == e[i]);
+
+	n = s.readAll(dst, 2);
+	assert(n == 2);
+	assert(s.size() == 13);
+	for (int i = 0; i < n; i++) assert(dst[i] == e[i + 1]);
+
+	n = s.readAll(dst, 3);
+	assert(n == 3);
+	assert(s.size() == 10);
+	for (int i = 0; i < n; i++) assert(dst[i] == e[i + 3]);
+
+	n = s.readAll(dst, 4);
+	assert(n == 4);
+	assert(s.size() == 6);
+	for (int i = 0; i < n; i++) assert(dst[i] == e[i + 6]);
+
+	// Now read everything possible.
+	n = s.readAll(dst, arraySize(dst));
+	assert(n == 6);
+	assert(s.size() == 0);
+	for (int i = 0; i < n; i++) assert(dst[i] == e[i + 10]);
+}
