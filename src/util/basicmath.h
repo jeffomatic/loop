9d02afd9cee3fd366385646cd937bacf2a72835e
Jeff Lee <jeffomatic@gmail.com>
2015-06-14 15:04:32 -0700

    Fix non-template funcs in basicmath.h

--- src/util/basicmath.h
+++ src/util/basicmath.h
@@ -3,19 +3,19 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-bool wrapLess(uint8_t a, uint8_t b) {
+inline bool wrapLess(uint8_t a, uint8_t b) {
 	return (int8_t)(a - b) < 0;
 }
 
-bool wrapLess(uint16_t a, uint16_t b) {
+inline bool wrapLess(uint16_t a, uint16_t b) {
 	return (int16_t)(a - b) < 0;
 }
 
-bool wrapLess(uint32_t a, uint32_t b) {
+inline bool wrapLess(uint32_t a, uint32_t b) {
 	return (int32_t)(a - b) < 0;
 }
 
-bool wrapLess(uint64_t a, uint64_t b) {
+inline bool wrapLess(uint64_t a, uint64_t b) {
 	return (int64_t)(a - b) < 0;
 }
 
